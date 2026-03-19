464ecde82affe6127eef93b5681368b8e08cb4ed
PolinaDubova <pharabadot@gmail.com>
2026-03-19 14:06:36 +0100

    front commit

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://unadvisedly-bibliopegic-sharon.ngrok-free.dev",
   timeout: 1000,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  },
 });
 
 instance.interceptors.request.use(
